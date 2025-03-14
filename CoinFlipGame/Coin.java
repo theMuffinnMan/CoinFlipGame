f845b9134dd342ac03eed38528554ee4c546b936
theMuffinnMan <164093037+theMuffinnMan@users.noreply.github.com>
2025-03-14 22:01:30 +1300

    CoinFlipGame - upload for classroom

---
+++ CoinFlipGame/Coin.java
@@ -0,0 +1,82 @@
+import java.util.Random;
+/**
+ * Write a description of class Coin here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Coin
+{
+    // instance variables - replace the example below with your own
+    private String side;
+    private String colour;
+    private int faceValue;
+    private int points;
+    private String face;
+
+    /**
+     * Constructor for objects of class Coin
+     */
+    public Coin(String Colour)
+    {
+        // initialise instance variables
+        colour = Colour;
+        points = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void flip()
+    {
+        //get random number which corrosponds to coin side
+        Random rand = new Random();
+    
+        faceValue = rand.nextInt(2);
+        if (faceValue == 0){
+            awardPoints();
+        }
+    }
+    /*
+     * set face value
+     */
+    public void setSide(int value){
+        
+    }
+    
+    /**
+     * faceValue getter
+     */
+    public String getFaceValue(){
+        if(faceValue == 0){
+            return "HEADS";
+        }
+        else{
+            return "TAILS";
+        }
+    }
+    
+    /**
+     * COLOUR GETTER getter
+     */
+    public String getColour(){
+        return colour;
+    } 
+    
+    /**
+     * points getter
+     */
+    public int getPoints(){
+        return points;
+    }
+
+    /**
+     * points awarding
+     */
+    public void awardPoints(){
+        points += 1;
+    }
+}
---
+++ CoinFlipGame/main.java
@@ -0,0 +1,52 @@
+
+/**
+ * Write a description of class main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class main
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class main
+     */
+    public static void main (String[] args)
+    {
+        // initialise instance variables
+        Coin coin1 = new Coin("Gold");
+        Coin coin2 = new Coin("Silver");
+        
+        System.out.println("Gold coin Vs. Silver coin \nFirst to 3 heads wins");
+        
+        while(coin1.getPoints() < 3 && coin2.getPoints() < 3){
+            coin1.flip();
+            coin2.flip();
+            System.out.println(coin1.getColour() + " : " + coin1.getFaceValue());
+            System.out.println(coin2.getColour() + " : " + coin2.getFaceValue());
+            System.out.println(coin1.getColour() + " : " + coin1.getPoints() + " Points");
+            System.out.println(coin2.getColour() + " : " + coin2.getPoints() + " Points");
+            System.out.println();
+            if(coin1.getPoints() == 3 && coin2.getPoints() < 3){
+                System.out.println("Gold Coin wins");
+            }
+            else if(coin2.getPoints() == 3 && coin1.getPoints() < 3){
+                System.out.println("Silver Coin wins");
+            }
+            else if(coin1.getPoints() == 3 && coin2.getPoints() == 3){
+                System.out.println("Its a draw!");
+            }
+        }
+    }
+
+    /**
+     * 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
